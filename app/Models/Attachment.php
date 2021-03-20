f3512ec9e36bc20331805549771c47ab3e747a4c
João Cruz <joao.fs.cruz97@gmail.com>
2021-03-20 19:23:55 +0000

    Add accessors to models

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Attachment extends Model
 {
@@ -12,6 +13,42 @@ class Attachment extends Model
     protected $fillable = [
         'filepath',
     ];
+
+    protected $appends = [
+        'filename',
+        'filesize'
+    ];
+
+    /**
+     * Returns the filename derived from the filepath.
+     *
+     * @return mixed|string
+     */
+    public function getFilenameAttribute()
+    {
+        $splitPath = explode('/', $this->filepath);
+        return end($splitPath);
+    }
+
+    /**
+     * Returns the file size in with the appropriate size unit.
+     *
+     * @return mixed|string
+     */
+    public function getFilesizeAttribute()
+    {
+        $size = Storage::size($this->filepath);
+        $sizeUnit = '';
+        if ($size >= 524288) {
+            $size = number_format($size / 1048576, 2) . ' MB';
+        } elseif ($size >= 512) {
+            $size = number_format($size / 1024, 2) . ' KB';
+        } else {
+            $sizeUnit = ' bytes';
+        }
+
+        return $size . $sizeUnit;
+    }
 
     /**
      * Get the email this attachment belongs to.
--- app/Models/Email.php
+++ app/Models/Email.php
@@ -17,6 +17,40 @@ class Email extends Model
         'html',
     ];
 
+    protected $appends = [
+        'status',
+        'posted_at'
+    ];
+
+    /**
+     * Get the email status.
+     *
+     * @return string
+     */
+    public function getStatusAttribute(): string
+    {
+        switch ($this->attributes['status']) {
+            case 'posted':
+                return 'Posted';
+            case 'sent':
+                return 'Sent';
+            case 'failed':
+                return 'Failed';
+        }
+
+        return 'Unknown';
+    }
+
+    /**
+     * Get the time that has passed since the email was posted.
+     *
+     * @return mixed
+     */
+    public function getPostedAtAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
     /**
      * Get the attachments this email owns.
      */
